c6610b5884cc071aae262b77531715af2c91dd93
PedroShady19 <pmff.pedro19999@gmail.com>
2018-04-19 13:41:30 +0100

    commit2

--- app/src/main/java/com/productions/esaf/cafe/MainActivity.java
+++ app/src/main/java/com/productions/esaf/cafe/MainActivity.java
@@ -1,20 +1,74 @@
 package com.productions.esaf.cafe;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    //Variaveis
+    private Button btnChooseImage,btnUploadImage;
+    private ImageView imageView;
+
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST=71;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Init View
+        btnChooseImage= findViewById(R.id.btnChooseImage);
+        btnUploadImage=findViewById(R.id.btnUploadImage);
+        imageView=findViewById(R.id.imageView);
+
+        btnChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+        btnUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void chooseImage() {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"),PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==PICK_IMAGE_REQUEST && resultCode==RESULT_OK
+                && data !=null && data.getData() !=null)
+        {
+            filePath=data.getData();
+            try{
+                Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
 
     }
 
@@ -41,7 +95,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void addFoodButton(View view) {
 
-    }
 }
